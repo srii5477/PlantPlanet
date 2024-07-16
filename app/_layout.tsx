5f4f8c4cac1bc93b7ab6e0fb433b2b110388b265
Sride Shankar <sridevishankar1161@gmail.com>
2024-07-16 10:36:23 +0530

    initialize project structure

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,37 +1,20 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import { StyleSheet, Text, View } from 'react-native'
+import { Slot } from 'expo-router';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Index from './(tabs)/index';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+const Stack = createNativeStackNavigator();
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
-  );
-}
+    <>
+    <Text> Hello </Text>
+    <Stack.Navigator>
+      <Stack.Screen name="index" component={Index} options={{ headerShown: false }} />
+    </Stack.Navigator>
+    <Text> Sridevi </Text>
+    </>
+  )
+} 
