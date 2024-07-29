0913f52484615e27dcace8f2e49cd580bd18de7c
Sride Shankar <sridevishankar1161@gmail.com>
2024-07-29 10:25:17 +0530

    fix pressable bug

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,58 +1,74 @@
-import { Text, View, Button, StyleSheet, TouchableOpacity } from 'react-native'
+import { Text, View, Button, Pressable, StyleSheet, TouchableOpacity, ImageBackground, SafeAreaView } from 'react-native'
 import React from 'react'
 import { Slot, Link } from 'expo-router';
 import { useNavigation } from '@react-navigation/native';
+
 
 
 const Index = () => {
   const navigation = useNavigation();
   return (
     <View className="flex-1 items-center justify-center bg-emerald-300">
-      <Text className="text-emerald-700 text-4xl font-plight">Welcome to PlantPlanet</Text>
-      <TouchableOpacity>
-      <Button
-          title="View Profile"
-          onPress={() =>
-            navigation.navigate('Profile')
-          }
-          
-      />
-      </TouchableOpacity>
-      <TouchableOpacity>
-        <Button
-          title="Access Feed"
-          onPress={() =>
-            navigation.navigate('Feed')
-          }
-        />
-      </TouchableOpacity>
-      <TouchableOpacity>
-        <Button
-          title="Explore"
-          onPress={() =>
-            navigation.navigate('Explore')
-          }
-        />
-      </TouchableOpacity>
-      <TouchableOpacity>
-        <Button
-          title="Map"
-          onPress={() =>
-            navigation.navigate('Map')
-          }
-        />
-      </TouchableOpacity>
-      <TouchableOpacity>
-        <Button
-          title="Quiz"
-          onPress={() =>
-            navigation.navigate('Quiz')
-          }
-        />
-      </TouchableOpacity>
+      <ImageBackground source={ require('../../assets/images/3.jpg') } style={styles.container}>
+        <Text className="text-emerald-100 text-4xl font-plight m-40 text-center">Welcome to  {"\n"} PlantPlanet</Text>
+        
+        <Pressable
+            onPress={() =>
+              navigation.navigate('Profile')
+            }
+            style={styles.button}
+        ><Text style={styles.text}>View Profile</Text></Pressable>
+        
+        <TouchableOpacity>
+          <Button
+            title="Access Feed"
+            onPress={() =>
+              navigation.navigate('Feed')
+            }
+          />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Button
+            title="Explore"
+            onPress={() =>
+              navigation.navigate('Explore')
+            }
+          />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Button
+            title="Map"
+            onPress={() =>
+              navigation.navigate('Map')
+            }
+          />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Button
+            title="Quiz"
+            onPress={() =>
+              navigation.navigate('Quiz')
+            }
+          />
+        </TouchableOpacity>
+      </ImageBackground>
     </View>
   )
 }
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    height: 900, 
+    width: 800
+  },
+  text: {
+    color: 'white'
+  },
+  button: {
+    backgroundColor: 'transparent',
+  }
+})
 
 export default Index
 
--- app/(tabs)/quiz.tsx
+++ app/(tabs)/quiz.tsx
@@ -6,12 +6,22 @@ import Bank from './bank'
 const Quiz = () => {
   let [ current, setCurrent ] = useState(0);
   let [ timer, setTimer ] = useState(10); 
+  let [ score, setScore ] = useState(0);
   let [ hasStarted, setHasStarted ] = useState(false);
-
+  let arr = []
+  for (let i = 0; i < Bank.length; i++) {
+    arr.push(Bank[i].options);
+  }
   function startQuiz () {
-    let newValue = !hasStarted;
-    setHasStarted(newValue);
+    setHasStarted(true);
     // generate report
+  }
+  function generateScore () {
+
+  }
+  function endQuiz () {
+    setHasStarted(false);
+    generateScore();
   }
 
   useEffect(() => {
@@ -33,18 +43,24 @@ const Quiz = () => {
   
 
   return (
-    <ScrollView className="bg-emerald-600">
-      <Text> Quiz </Text>
+    <ScrollView className="bg-emerald-300">
+      <Text className="font-pthin text-4xl text-center m-5"> Quiz </Text>
       {/* <TouchableOpacity onPress={startQuiz}> <Text> Start/end the quiz. </Text> </TouchableOpacity> */}
-      <Button loading={true} className="w-80 m-8 bg-emerald-700" buttonColor="#003300" mode="contained-tonal" onPress={startQuiz} title="Start/end the quiz">
+      <Button loading={true} className="w-80 m-8 bg-emerald-700" buttonColor="#003300" mode="contained-tonal" onPress={startQuiz} title="Start the quiz">
       </Button>
-      { hasStarted ?
+      { hasStarted && current < Bank.length ?
         <View>
-            <Text> Question Card </Text>
-            <Text> { Bank[current]['description'] } </Text>
-            { Bank.map((info, key) => }
-        </View> : <Text> xyz </Text>
+            <Text className="font-pmedium text-2xl text-emerald-800 m-5 text-center"> Question Card </Text>
+            <Text className="font-pbold text-lg text-emerald-800"> { current + 1 }) { Bank[current]['description'] } </Text>
+            <Text className="font-pregular text-base text-emerald-800 text-center m-5"> Time remaining: { timer } </Text>
+            { arr[current].map((info, key) => <Text className="font-pregular text-sm text-emerald-800"> Option {key+1}: {info} </Text>)  }
+            
+        </View> : 
+        <View>
+          <Text className="font-pbold text-sm text-emerald-800"> Prepare yourself... </Text>
+        </View>
       } 
+      <Button loading={true} className="w-80 m-8 bg-emerald-700" buttonColor="#003300" mode="contained-tonal" onPress={endQuiz} title="End the quiz"></Button>
     </ScrollView>
   )
 }
